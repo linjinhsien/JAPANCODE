5ceac59effe70d4b8969d10c820f75002d0c98dc
iven <iven840320@gmail.com>
2024-05-17 18:18:14 +0800

    加入專案檔案。

---
+++ TRY1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TRY1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2;
+
+            Console.WriteLine("請輸入第一個數字：");
+            while (!int.TryParse(Console.ReadLine(), out x1))
+            {
+                Console.WriteLine("無效的輸入。請輸入一個數字。");
+            }
+
+            Console.WriteLine("請輸入第二個數字：");
+            while (!int.TryParse(Console.ReadLine(), out x2))
+            {
+                Console.WriteLine("無效的輸入。請輸入一個數字。");
+            }
+
+            int result = x1 * x2;
+            Console.WriteLine($"兩數的乘積是：{result}");
+
+            // 調用 Calculate_CharCount 方法
+            Calculate_CharCount();
+        }
+
+        // 將 Calculate_CharCount 方法移至 Main 方法外部
+        static void Calculate_CharCount()
+        {
+            Console.WriteLine("請輸入一行文字：");
+            string? line = Console.ReadLine();
+            int charCount = line?.Length ?? 0;
+            Console.WriteLine($"{charCount} 是文字的長度。");
+        }
+    }
+}
---
+++ TRY2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+string html = "<html><head></head><body>"
+    + "<div style=\"width:100%;height:80px;background-color:#f08080;color:#ffffff;\">mediumvioletred</div>"
+    + "<div style=\"width:100%;height:80px;background-color:#ff0000\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#ff8800\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#ffff00\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#22dd00\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#0000cc\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#cc00cc\"></div>"
+    + "<div style=\"width:100%;height:80px;background-color:#ff00aa\"></div>"
+    + "</body></html>";
+
+File.WriteAllText("C:\\Data\\MyHomepage.html", html, Encoding.UTF8);
+
+Console.WriteLine("HTML檔案已經建立！");
+
+CreateTextFile();
+
+static void CreateTextFile()
+{
+    File.WriteAllText("C:\\Data\\MyFavoriteFoods.txt", "瑪麗喜歡的食物。法式吐司、漢堡、烤肉。", Encoding.UTF8);
+}
